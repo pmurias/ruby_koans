c53a9aed5eb17558a16c687369e39f0c7a6a9b18
Pawel Murias <pawelmurias@gmail.com>
2014-10-12 22:59:51 +0200

    Pass some tests.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 or b <= 0 or c <= 0
+     raise TriangleError
+  end
+  if a >= b+c or b >= a+c or c >= a+b
+     raise TriangleError
+  end
+  if a == b
+     if b == c
+        :equilateral
+     else
+        :isosceles
+     end
+  elsif b == c or a == c
+     :isosceles
+  else
+     :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
